edab28a88bdeed9fffb29ab879b9e7630384e507
Leo <Leo@PC>
2020-05-14 17:39:57 +0100

    Removed old folders, merging fork into master repo

--- ProjectMapSite/includes/logout.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Created by Leonardo
-
-    session_start(); //Resumes session
-
-    session_destroy(); // Ends session
-
-    header('Location: /'); // Returns to home page
--- ProjectMapSite/templates/main.php
+++
@@ -1,9 +0,0 @@
-<!-- Created by Leonardo -->
-<?php
-
-require __DIR__ . "/../vendor/autoload.php";
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'mapsystem');
